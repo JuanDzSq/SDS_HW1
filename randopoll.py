1e570cc7cab771279e3485a6f94ade9d67a67951
Juan Diaz <juan.dz@techexchange.in>
2023-02-02 06:07:59 +0000

    Uncommented ArgumentParser methods

--- randopoll.py
+++ randopoll.py
@@ -2,21 +2,21 @@ from argparse import ArgumentParser
 from poller import Poller
 from subprocess import run
 
-#parser = ArgumentParser(prog = "RandoPoll",
-#                    description = "What the program does")
-#parser.add_argument("filename")
+parser = ArgumentParser(prog = "RandoPoll",
+                    description = "What the program does")
+parser.add_argument("filename")
 
-#args = parser.parse_args()
+args = parser.parse_args()
 
 def main():
     locked = False
-    with Poller('participants.csv') as poller:    #args.filename
+    with Poller(args.filename) as poller:
         for participant in poller:
             while True:
                 print("%s: (A)nswered (C)orrect (E)xcused (M)issing (Q)uit" % participant)
                 command = input().lower()
                 if command == "a":
-                    poller.attempted() # participant.attempted() ?
+                    poller.attempted()
                     break
                 elif command == "c":
                     poller.correct()
